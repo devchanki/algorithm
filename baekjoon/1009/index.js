27d084ebf174fb464cab8a27f4bcf57c1d3c4d6c
devchanki <kgichan12@gmail.com>
2025-03-25 00:43:34 +0900

    1009 번 풀이 완료

---
+++ baekjoon/1009/index.js
@@ -0,0 +1,33 @@
+// 백준에서는 첫번째 조건으로 input 받게 설정
+// 내 맥북, desktop 에선 두번째걸로 선택됨
+const input =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+    : require("fs").readFileSync("./input.txt").toString().trim().split("\r\n");
+
+n = parseInt(input[0]);
+
+const getRepeatedLastNumberArr = (a, b) => {
+  const arr = [a % 10];
+  for (let i = 0; i < b; i++) {
+    const lastNumber = (arr[arr.length - 1] * a) % 10;
+    if (arr[0] === lastNumber) {
+      return arr;
+    } else arr.push(lastNumber);
+  }
+  return arr;
+};
+
+const getLastDataProcessingComputer = (a, b) => {
+  const lastNumberArray = getRepeatedLastNumberArr(a, b);
+  const index = b % lastNumberArray.length || lastNumberArray.length;
+  return lastNumberArray[index - 1] || 10;
+};
+
+const result = [];
+for (let i = 1; i <= n; i++) {
+  const [a, b] = input[i].split(" ").map((str) => parseInt(str));
+  result.push(getLastDataProcessingComputer(a, b));
+}
+
+console.log(result.join("\n"));
---
+++ baekjoon/1074/index.js
@@ -0,0 +1,6 @@
+// 백준에서는 첫번째 조건으로 input 받게 설정
+// 내 맥북, desktop 에선 두번째걸로 선택됨
+const input =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+    : require("fs").readFileSync("./input.txt").toString().trim().split("\r\n");
