1f5365fdff618cf1174890cdff4d554a62028b7b
devchanki <kgichan12@gmail.com>
2025-04-03 23:15:18 +0900

    7490

---
+++ baekjoon/7490/index.js
@@ -0,0 +1,37 @@
+// 백준에서는 첫번째 조건으로 input 받게 설정
+// 내 맥북, desktop 에선 두번째걸로 선택됨
+const input =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+    : require("fs").readFileSync("./input.txt").toString().trim().split("\r\n");
+
+N = parseInt(input[0]);
+const answer = [];
+
+const isSumIsZero = (currentNumberString) => {
+  return eval(currentNumberString.replaceAll(" ", "")) === 0;
+};
+
+const makeNumberString = (operatorArr, lastNumber, arr) => {
+  if (operatorArr.length === lastNumber - 1) {
+    const strArr = [];
+    for (let i = 1; i <= lastNumber; i++) {
+      strArr.push(i);
+      const operator = operatorArr.pop();
+      if (operator) strArr.push(operator);
+    }
+    if (isSumIsZero(strArr.join(""))) arr.push(strArr.join(""));
+    return;
+  }
+  makeNumberString([...operatorArr, "+"], lastNumber, arr);
+  makeNumberString([...operatorArr, "-"], lastNumber, arr);
+  makeNumberString([...operatorArr, " "], lastNumber, arr);
+};
+
+for (let i = 1; i <= N; i++) {
+  const arr = [];
+  makeNumberString([], parseInt(input[i]), arr);
+  arr.sort();
+  arr.forEach((el) => console.log(el));
+  console.log();
+}
